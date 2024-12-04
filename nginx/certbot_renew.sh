54b283d3e71beb56f107b923eb70ffbaea9516bf
s-hirano-ist <s-hirano-ist@outlook.com>
2024-12-04 21:19:37 +0900

    fix: renew command fail

--- nginx/certbot_renew.sh
+++ nginx/certbot_renew.sh
@@ -1,9 +1,9 @@
 docker run -it --rm --name certbot \
 -v /etc/letsencrypt:/etc/letsencrypt \
 -v /var/www/certbot:/var/www/certbot \
-certbot/certbot renew && docker-compose restart nginx
+certbot/certbot renew && docker restart nginx
 
 cp /etc/letsencrypt/live/private.s-hirano.com/privkey.pem /root/s-tools/s-private/minio/certs/private.key
 cp /etc/letsencrypt/live/private.s-hirano.com/fullchain.pem /root/s-tools/s-private/minio/certs/public.crt
 
-docker-compose restart s-storage
+docker restart s-storage
